ded31f7e2734db25d82b8b616757d5c85429e121
dkzks44 <75156747+dkzks44@users.noreply.github.com>
2020-12-18 23:05:17 +0900

    Add files via upload

--- week_03.py
+++ week_03.py
@@ -1,1 +1,27 @@
-    
+import requests
+from bs4 import BeautifulSoup
+
+# 타겟 URL을 읽어서 HTML를 받아오고,
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200713', headers=headers)
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+# soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
+# 이제 코딩을 통해 필요한 부분을 추출하면 된다.
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# select를 이용해서, tr들을 불러오기
+songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+    #태그 위치
+    # body-content > div.newest-list > div > table > tbody > tr:nthprint(song_rank.text[0:2].strip(), a_title.text.strip())-child(1) > td.number
+    # body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.title.ellipsis
+    #body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.artist.ellipsis
+for song in songs:
+    song_rank = song.select_one('td.number')
+    a_title = song.select_one('td.info > a.title.ellipsis')
+    singer = song.select_one('td.info > a.artist.ellipsis')
+    if song is not None :
+        print(song_rank.text[0:2].strip(), a_title.text.strip(), singer.text)
+
+
